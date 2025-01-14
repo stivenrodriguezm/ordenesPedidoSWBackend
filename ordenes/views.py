7c6bce807fc91033eaaaf3ab5237d4b0b928eb20
Stiven <73106994+stivenrodriguezm@users.noreply.github.com>
2025-01-13 23:14:06 -0500

    Update views.py

--- ordenes/views.py
+++ ordenes/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView, permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response  # Importación necesaria
 from django.http import HttpResponse, JsonResponse
 from .models import Referencia, Proveedor, OrdenPedido, DetallePedido
