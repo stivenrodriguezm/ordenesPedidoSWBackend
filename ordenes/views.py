fde5455df9bfbd366e6c63b6e5ddcb6baa7b46f2
Stiven <73106994+stivenrodriguezm@users.noreply.github.com>
2025-01-19 13:55:46 -0500

    Update views.py

--- ordenes/views.py
+++ ordenes/views.py
@@ -52,6 +52,14 @@ class OrdenPedidoViewSet(viewsets.ModelViewSet):
         # Agregar el contexto del request al serializer
         return {'request': self.request}
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
 
 class DetallePedidoViewSet(viewsets.ModelViewSet):
     queryset = DetallePedido.objects.all()
